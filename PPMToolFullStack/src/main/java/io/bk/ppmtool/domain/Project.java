80b456fb1211d531ab97f40b76259bd4659aca8a
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-09-27 22:44:30 +0700

    Project Repository

--- PPMToolFullStack/src/main/java/io/bk/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/domain/Project.java
@@ -1,4 +1,36 @@
 package io.bk.ppmtool.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate()
+    {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate()
+    {
+        this.updated_At = new Date();
+    }
+
 }
---
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.bk.ppmtool.repositories;
+
+import io.bk.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+
+}
