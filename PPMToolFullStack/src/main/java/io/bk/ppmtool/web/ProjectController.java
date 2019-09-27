5c63771f939c860f961e1b34cf300eaa6ae7d6f1
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-09-27 23:41:31 +0700

    Refactor Project Controller

--- PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
@@ -1,18 +1,18 @@
 package io.bk.ppmtool.web;
 
 import io.bk.ppmtool.domain.Project;
+import io.bk.ppmtool.services.MapValidationErrorService;
 import io.bk.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,18 +21,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result)
-    {
-        if (result.hasErrors())
-        {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error: result.getFieldErrors())
-            {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap,HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
         }
+
         Project projectCreated = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(projectCreated, HttpStatus.CREATED);
     }
