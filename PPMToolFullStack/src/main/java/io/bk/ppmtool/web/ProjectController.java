70351879d7891c14aafcedb4eed67b02a125bc39
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-09-27 23:21:05 +0700

    Project Valid path 3 - notblank

--- PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +26,12 @@ public class ProjectController {
     {
         if (result.hasErrors())
         {
-            return new ResponseEntity<String>("Invalid project object",HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error: result.getFieldErrors())
+            {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap,HttpStatus.BAD_REQUEST);
         }
         Project projectCreated = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(projectCreated, HttpStatus.CREATED);
