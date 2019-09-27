542bb73c16e299286ee04f6730a01bc4e8e58bdd
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-09-27 23:08:07 +0700

    Project Valid path 1

--- PPMToolFullStack/src/main/java/io/bk/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/domain/Project.java
@@ -1,8 +1,11 @@
 package io.bk.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -12,13 +15,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message="Project name is required")
     private String projectName;
+
+    @NotBlank(message="Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4-5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message="Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     @PrePersist
