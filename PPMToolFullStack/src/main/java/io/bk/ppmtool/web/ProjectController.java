c9ae4fde85c9b159d1a8795a3814db2485eedb68
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-09-27 23:13:05 +0700

    Project Valid path 2

--- PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/bk/ppmtool/web/ProjectController.java
@@ -5,7 +5,10 @@ import io.bk.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,8 +18,12 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project)
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result)
     {
+        if (result.hasErrors())
+        {
+            return new ResponseEntity<String>("Invalid project object",HttpStatus.BAD_REQUEST);
+        }
         Project projectCreated = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(projectCreated, HttpStatus.CREATED);
     }
